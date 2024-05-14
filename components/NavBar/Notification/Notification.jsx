22af849c16668a21db31909091f70ca8337d1fd6
ZeroToH3ro <mtblaser2002@gmail.com>
2024-05-14 14:07:44 +0700

    setup smart contract

---
+++ components/NavBar/Notification/Notification.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// INTERNAL IMPORT
+import Style from './Notification.module.css'
+const Notification = () => {
+    return <div>Notification</div>
+};
+
+export default Notification
---
+++ components/NavBar/SideBar/SideBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// INTERNAL IMPORT
+import Style from './SideBar.module.css'
+const SideBar = () => {
+    return <div>SideBar</div>
+};
+
+export default SideBar
